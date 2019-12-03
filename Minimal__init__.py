cc3f4183ee9d449692ab10c8a5c109c23e308307
Zebfred <zebfred22@gmail.com>
2019-12-02 18:34:11 -0800

    This is everything

---
+++ Minimal__init__.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+""""
+Lambdata - a collection of data science helper functions
+""""
+
+ONES = pd.DataFrame(np.ones(10))
+
+ZEROS = pd.DataFrame(np.zeros(50))
